a667ad004bf0929ebedf18f9c9f91921897c47be
gayathrikk <gayathri@htic.iitm.ac.in>
2023-10-19 14:18:24 +0530

    first commit

---
+++ src/main/java/Sanity/Atlas/App.java
@@ -0,0 +1,13 @@
+package Sanity.Atlas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
