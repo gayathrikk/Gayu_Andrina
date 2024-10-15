00115e09ed783ea13af6edad58a76e44f95500d8
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-10-15 13:11:17 +0530

    Andrina_Vote.java

--- src/test/java/Sanity/Atlas/Andrina_Vote.java
+++ src/test/java/Sanity/Atlas/Andrina_Vote.java
@@ -1,41 +1,61 @@
 package Sanity.Atlas;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Andrina_Vote {
 
-    public static void main(String[] args) throws InterruptedException {
-      
-        WebDriverManager.chromedriver().setup();
+    WebDriver driver;
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-notifications"); 
+    // Setup method to initialize the remote WebDriver
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        // DesiredCapabilities for remote Chrome execution
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
 
-        WebDriver driver = new ChromeDriver(options);
+        // URL for the Selenium Grid Hub
+        URL url = new URL("http://172.20.23.92:4443/wd/hub");
 
+        // Initialize the remote WebDriver with the capabilities and grid URL
+        driver = new RemoteWebDriver(url, dc);
+    }
+
+    @Test
+    public void voteTest() throws InterruptedException {
+        
         driver.get("https://mycutebaby.in/contest/participant/6707006347bd2?utm_medium=w_10_24#google_vignette");
 
+        driver.manage().window().maximize();
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Page Title: " + driver.getTitle());
+
+        
         Thread.sleep(2000);
 
-        // Scroll down by a certain number of pixels
+        
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,2000)", "");
+        js.executeScript("window.scrollBy(0, 1800)", "");
+        Thread.sleep(2000);
 
-       driver.findElement(By.xpath("//a[@id='vote_btn']")).click();
+        
+        driver.findElement(By.xpath("//a[@id='vote_btn']")).click();
+
+       
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        // Close the browser after voting
         driver.quit();
     }
 }
